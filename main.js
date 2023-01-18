c7cdb6c64f8bf4d34af44f5f137321c4951fc338
yankacst <yankacastrov@gmail.com>
2023-01-18 19:04:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const fNameInput = document.querySelector(".fName");
+const fEmailInput = document.querySelector(".fEmail");
+const fPhoneInput = document.querySelector(".fPhone");
+const fPasswordInput = document.querySelector(".fPassword");
